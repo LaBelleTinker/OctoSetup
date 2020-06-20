ffe15dde63bc5bb57fdd10417980a9c8c70aba1c
Jeff Collins <jeff.andrew.collins@gmail.com>
2020-06-19 21:59:33 -0700

    revert to python2

--- octosetup.sh
+++ octosetup.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
 echo -e "\e[1m\e[36mInstalling requirements\e[0m\e[39m"
 sudo apt-get update
-sudo apt-get -qqy install python3-pip python3-dev python3-setuptools python3-virtualenv git libyaml-dev build-essential haproxy subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake avahi-daemon
-sudo pip3 install virtualenv
+sudo apt-get -qqy install python-pip python-dev python-setuptools python-virtualenv git libyaml-dev build-essential haproxy subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake avahi-daemon
 echo -e "\e[1m\e[36mCloning OctoPrint\e[0m\e[39m"
 git clone https://github.com/foosel/OctoPrint.git
 
 echo -e "\e[1m\e[36mCreating virtualenv\e[0m\e[39m"
-python3 -m venv /home/pi/Aquarium
+virtualenv /home/pi/Aquarium
 cd OctoPrint
 echo -e "\e[1m\e[36mInstalling Octoprint\e[0m\e[39m"
 /home/pi/Aquarium/bin/python3 setup.py -q install 
 cd ..
 sudo rm -rf OctoPrint
-/home/pi/Aquarium/bin/pip3 -qq install pip --upgrade
+/home/pi/Aquarium/bin/pip -qq install pip --upgrade
 echo -e "\e[1m\e[36mInstalling Bed Level Visualizer plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip3 -qq install "https://github.com/jneilliii/OctoPrint-BedLevelVisualizer/archive/master.zip"
+/home/pi/Aquarium/bin/pip -qq install "https://github.com/jneilliii/OctoPrint-BedLevelVisualizer/archive/master.zip"
 echo -e "\e[1m\e[36mInstalling Cost Estimation plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip3 -qq install "https://github.com/malnvenshorn/OctoPrint-CostEstimation/archive/master.zip"
+/home/pi/Aquarium/bin/pip -qq install "https://github.com/malnvenshorn/OctoPrint-CostEstimation/archive/master.zip"
 echo -e "\e[1m\e[36mInstalling Filament Manager plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip3 -qq install "https://github.com/malnvenshorn/OctoPrint-FilamentManager/archive/master.zip"
+/home/pi/Aquarium/bin/pip -qq install "https://github.com/malnvenshorn/OctoPrint-FilamentManager/archive/master.zip"
 echo -e "\e[1m\e[36mInstalling File Manager plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip3 -qq install "https://github.com/Salandora/OctoPrint-FileManager/archive/master.zip"
+/home/pi/Aquarium/bin/pip -qq install "https://github.com/Salandora/OctoPrint-FileManager/archive/master.zip"
 echo -e "\e[1m\e[36mInstalling Layer Display plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip3 -qq install "https://github.com/chatrat12/layerdisplay/archive/master.zip"
+/home/pi/Aquarium/bin/pip -qq install "https://github.com/chatrat12/layerdisplay/archive/master.zip"
 echo -e "\e[1m\e[36mInstalling Cancel Object plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip3 -qq install "https://github.com/paukstelis/OctoPrint-Cancelobject/archive/master.zip"
+/home/pi/Aquarium/bin/pip -qq install "https://github.com/paukstelis/OctoPrint-Cancelobject/archive/master.zip"
 echo -e "\e[1m\e[36mInstalling ETA plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip3 -qq install "https://github.com/pablogventura/Octoprint-ETA/archive/master.zip"
+/home/pi/Aquarium/bin/pip -qq install "https://github.com/pablogventura/Octoprint-ETA/archive/master.zip"
 echo -e "\e[1m\e[36mInstalling Print Time Genius plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip3 -qq install "https://github.com/eyal0/OctoPrint-PrintTimeGenius/archive/master.zip"
+/home/pi/Aquarium/bin/pip -qq install "https://github.com/eyal0/OctoPrint-PrintTimeGenius/archive/master.zip"
 echo -e "\e[1m\e[36mInstalling PrintTrack plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip3 -qq install "https://github.com/ElectricSquid/OctoPrint-PrintTrack/archive/master.zip"
+/home/pi/Aquarium/bin/pip -qq install "https://github.com/ElectricSquid/OctoPrint-PrintTrack/archive/master.zip"
 echo -e "\e[1m\e[36mInstalling OctoLapse plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip3 -qq install "https://github.com/FormerLurker/Octolapse/archive/master.zip"
+/home/pi/Aquarium/bin/pip -qq install "https://github.com/FormerLurker/Octolapse/archive/master.zip"
 echo -e "\e[1m\e[36mInstalling Preheat Button plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip3 -qq install "https://github.com/marian42/octoprint-preheat/archive/master.zip"
+/home/pi/Aquarium/bin/pip -qq install "https://github.com/marian42/octoprint-preheat/archive/master.zip"
 echo -e "\e[1m\e[36mInstalling TouchUI plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip3 -qq install "https://github.com/BillyBlaze/OctoPrint-TouchUI/archive/master.zip"
+/home/pi/Aquarium/bin/pip -qq install "https://github.com/BillyBlaze/OctoPrint-TouchUI/archive/master.zip"
 echo -e "\e[1m\e[36mInstalling OctoKlipper plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip3 -qq install "https://github.com/mmone/OctoprintKlipperPlugin/master.zip"
+/home/pi/Aquarium/bin/pip -qq install "https://github.com/mmone/OctoprintKlipperPlugin/master.zip"
 
 echo -e "\e[1m\e[36mSetting up autostart\e[0m\e[39m"
 mkdir /home/pi/.config/autostart
