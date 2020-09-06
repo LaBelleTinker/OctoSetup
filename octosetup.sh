8496d88f52e2b9ecb5fa5afc758571f9857c0f75
Jeff Collins <jeff.andrew.collins@gmail.com>
2020-09-06 13:27:00 -0700

    Another update

--- octosetup.sh
+++ octosetup.sh
@@ -1,64 +1,106 @@
 #!/bin/bash
+#Environment Variables:
+#    SETUP_VIRTUALENV
+#    INSTALL_OCTOPRINT
+#    INSTALL_PLUGINS
+#    SETUP_AUTOSTART
+#    INSTALL_KLIPPER
+#    FLASH_PRINTER (INSTALL_KLIPPER must not be 'false'
+#    INSTALL_PICAM
+#    SETUP_LOCAL_ACCESS
+pushd .
+echo $INSTALL_OCTOPRINT
 echo "\e[1m\e[36mInstalling requirements\e[0m\e[39m"
 sudo apt-get -qq update
-sudo apt-get -qqy install python-pip python-dev python-setuptools python-virtualenv git libyaml-dev build-essential haproxy subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake avahi-daemon
-echo "\e[1m\e[36mSetting up virtualenv\e[0m\e[39m"
-virtualenv -q /home/pi/Aquarium
-/home/pi/Aquarium/bin/pip -qq install pip --upgrade
-/home/pi/Aquarium/bin/pip -qq install -Iv rsa==4.0
-echo "\e[1m\e[36mInstalling Octoprint\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip -qq install octoprint 
-echo "\e[1m\e[36mInstalling Bed Level Visualizer plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip -qq install "https://github.com/jneilliii/OctoPrint-BedLevelVisualizer/archive/master.zip"
-echo "\e[1m\e[36mInstalling Cost Estimation plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip -qq install "https://github.com/malnvenshorn/OctoPrint-CostEstimation/archive/master.zip"
-echo "\e[1m\e[36mInstalling Filament Manager plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip -qq install "https://github.com/malnvenshorn/OctoPrint-FilamentManager/archive/master.zip"
-echo "\e[1m\e[36mInstalling File Manager plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip -qq install "https://github.com/Salandora/OctoPrint-FileManager/archive/master.zip"
-echo "\e[1m\e[36mInstalling Layer Display plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip -qq install "https://github.com/chatrat12/layerdisplay/archive/master.zip"
-echo "\e[1m\e[36mInstalling Cancel Object plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip -qq install "https://github.com/paukstelis/OctoPrint-Cancelobject/archive/master.zip"
-echo "\e[1m\e[36mInstalling ETA plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip -qq install "https://github.com/pablogventura/Octoprint-ETA/archive/master.zip"
-echo "\e[1m\e[36mInstalling Print Time Genius plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip -qq install "https://github.com/eyal0/OctoPrint-PrintTimeGenius/archive/master.zip"
-echo "\e[1m\e[36mInstalling PrintTrack plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip -qq install "https://github.com/ElectricSquid/OctoPrint-PrintTrack/archive/master.zip"
-echo "\e[1m\e[36mInstalling OctoLapse plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip -qq install "https://github.com/FormerLurker/Octolapse/archive/master.zip"
-echo "\e[1m\e[36mInstalling Preheat Button plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip -qq install "https://github.com/marian42/octoprint-preheat/archive/master.zip"
-echo "\e[1m\e[36mInstalling OctoKlipper plugin\e[0m\e[39m"
-/home/pi/Aquarium/bin/pip -qq install "https://github.com/mmone/OctoPrintKlipper/archive/master.zip"
-
-echo "\e[1m\e[36mSetting up autostart\e[0m\e[39m"
-[ ! -d /home/pi/.config/autostart ] && mkdir /home/pi/.config/autostart
-sudo cp tightvnc.desktop /home/pi/.config/autostart
-sudo cp octoprint.init /etc/init.d/octoprint
-sudo cp octoprint.default /etc/default/octoprint
-echo "\e[1m\e[36mSetting up webcam\e[0m\e[39m"
-[ ! -d /home/pi/scripts ] && mkdir /home/pi/scripts
-sudo cp webcam /home/pi/scripts
-sudo cp webcamdaemon /home/pi/scripts
-sudo chmod +x /etc/init.d/octoprint
-sudo chmod +x /home/pi/scripts/webcam
-sudo chmod +x /home/pi/scripts/webcamdaemon
-sudo cp haproxy.cfg /etc/haproxy/haproxy.cfg
-[ ! -d /home/pi/.octoprint ] && mkdir /home/pi/.octoprint
-cat yamladdon >> /home/pi/.octoprint/config.yaml
-echo "\e[1m\e[36mCloning mjpg streamer\e[0m\e[39m"
-git clone -q https://github.com/jacksonliam/mjpg-streamer.git /home/pi/mjpg-streamer
-echo "\e[1m\e[36mInstalling mjpg streamer\e[0m\e[39m"
-cd /home/pi/mjpg-streamer/mjpg-streamer-experimental
-export LD_LIBRARY_PATH=.
-make --silent
-sudo make --silent install
-cd /home/pi
-sudo rm -rf mjpg-streamer
-echo "\e[1m\e[36mSetting up accesss via https://aquarium.local\e[0m\e[39m"
-sudo hostnamectl set-hostname aquarium
-[ ! -f /etc/hostname ] && sudo echo "aquarium" > /etc/hostname
-sudo sed -i 's/raspberrypi/aquarium/g' /etc/hosts
+sudo apt-get -qqy install python-pip python-dev python-setuptools python-virtualenv git libyaml-dev build-essential haproxy subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake avahi-daemon wget gzip tar libjpeg8-dev imagemagick libv4l-dev virtualenv libffi-dev libncurses-dev libusb-dev avrdude gcc-avr binutils-avr avr-libc stm32flash dfu-util libnewlib-arm-none-eabi gcc-arm-none-eabi binutils-arm-none-eabi libusb-1.0
+if [ "$SETUP_VIRTUALENV" != 'false' ]; then
+    echo "\e[1m\e[36mSetting up virtualenv\e[0m\e[39m"
+    virtualenv -q /home/pi/Aquarium
+    /home/pi/Aquarium/bin/pip -qq install pip --upgrade
+    /home/pi/Aquarium/bin/pip -qq install -Iv rsa==4.0
+fi
+if [ "$INSTALL_OCTOPRINT" != 'false' ]; then
+    echo "\e[1m\e[36mInstalling Octoprint\e[0m\e[39m"
+    /home/pi/Aquarium/bin/pip -qq install octoprint
+fi 
+if [ "$INSTALL_PLUGINS" != 'false' ]; then
+    echo "\e[1m\e[36mInstalling Bed Level Visualizer plugin\e[0m\e[39m"
+    /home/pi/Aquarium/bin/pip -qq install "https://github.com/jneilliii/OctoPrint-BedLevelVisualizer/archive/master.zip"
+    echo "\e[1m\e[36mInstalling Cost Estimation plugin\e[0m\e[39m"
+    /home/pi/Aquarium/bin/pip -qq install "https://github.com/malnvenshorn/OctoPrint-CostEstimation/archive/master.zip"
+    echo "\e[1m\e[36mInstalling Filament Manager plugin\e[0m\e[39m"
+    /home/pi/Aquarium/bin/pip -qq install "https://github.com/malnvenshorn/OctoPrint-FilamentManager/archive/master.zip"
+    echo "\e[1m\e[36mInstalling File Manager plugin\e[0m\e[39m"
+    /home/pi/Aquarium/bin/pip -qq install "https://github.com/Salandora/OctoPrint-FileManager/archive/master.zip"
+    echo "\e[1m\e[36mInstalling Layer Display plugin\e[0m\e[39m"
+    /home/pi/Aquarium/bin/pip -qq install "https://github.com/chatrat12/layerdisplay/archive/master.zip"
+    echo "\e[1m\e[36mInstalling Cancel Object plugin\e[0m\e[39m"
+    /home/pi/Aquarium/bin/pip -qq install "https://github.com/paukstelis/OctoPrint-Cancelobject/archive/master.zip"
+    echo "\e[1m\e[36mInstalling ETA plugin\e[0m\e[39m"
+    /home/pi/Aquarium/bin/pip -qq install "https://github.com/pablogventura/Octoprint-ETA/archive/master.zip"
+    echo "\e[1m\e[36mInstalling Print Time Genius plugin\e[0m\e[39m"
+    /home/pi/Aquarium/bin/pip -qq install "https://github.com/eyal0/OctoPrint-PrintTimeGenius/archive/master.zip"
+    echo "\e[1m\e[36mInstalling PrintTrack plugin\e[0m\e[39m"
+    /home/pi/Aquarium/bin/pip -qq install "https://github.com/ElectricSquid/OctoPrint-PrintTrack/archive/master.zip"
+    echo "\e[1m\e[36mInstalling OctoLapse plugin\e[0m\e[39m"
+    /home/pi/Aquarium/bin/pip -qq install "https://github.com/FormerLurker/Octolapse/archive/master.zip"
+    echo "\e[1m\e[36mInstalling Preheat Button plugin\e[0m\e[39m"
+    /home/pi/Aquarium/bin/pip -qq install "https://github.com/marian42/octoprint-preheat/archive/master.zip"
+    echo "\e[1m\e[36mInstalling OctoKlipper plugin\e[0m\e[39m"
+    /home/pi/Aquarium/bin/pip -qq install "https://github.com/mmone/OctoPrintKlipper/archive/master.zip"
+fi
+if [ "$SETUP_AUTOSTART" != 'false' ]; then
+    echo "\e[1m\e[36mSetting up autostart\e[0m\e[39m"
+    cd $(dirname $(readlink -f $0))
+    [ ! -d /home/pi/.config/autostart ] && mkdir /home/pi/.config/autostart
+    sudo cp tightvnc.desktop /home/pi/.config/autostart
+    sudo cp octoprint.init /etc/init.d/octoprint
+    sudo cp octoprint.default /etc/default/octoprint
+    sudo update-rc.d octoprint default
+    echo "\e[1m\e[36mSetting up webcam\e[0m\e[39m"
+    [ ! -d /home/pi/scripts ] && mkdir /home/pi/scripts
+    sudo cp webcam /home/pi/scripts
+    sudo cp webcamdaemon /home/pi/scripts
+    sudo chmod +x /etc/init.d/octoprint
+    sudo chmod +x /home/pi/scripts/webcam
+    sudo chmod +x /home/pi/scripts/webcamdaemon
+    sudo cp haproxy.cfg /etc/haproxy/haproxy.cfg
+    [ ! -d /home/pi/.octoprint ] && mkdir /home/pi/.octoprint
+    cat yamladdon >> /home/pi/.octoprint/config.yamls
+fi
+if [ "$INSTALL_KLIPPER" != 'false' ]; then
+    echo "\e[1m\e[36mCloning Klipper\e[0m\e[39m"
+    git clone -q https://github.com/KevinOConnor/klipper /home/pi/klipper
+    echo "\e[1m\e[36mInstalling Klipper\e[0m\e[39m"
+    sed 's/apt-get/apt-get -qq/g' /home/pi/klipper/scripts/install-octopi.sh
+    bash /home/pi/klipper/scripts/install-octopi.sh
+    cp klipper.config /home/pi/klipper/.config
+    cd /home/pi/klipper
+    make --silent
+    USBID=$(ls /dev/serial/by-id/*)
+    if [ "$FLASH_PRINTER" != 'false' ]; then
+        echo "\e[1m\e[36mFlashing Klipper firmware to printer\e[0m\e[39m"
+        sudo service klipper stop
+        make flash FLASH_DEVICE=$USBID
+        sudo service klipper start
+    fi
+fi
+if [ "$INSTALL_PICAM" != 'false' ]; then
+    echo "\e[1m\e[36mCloning mjpg streamer\e[0m\e[39m"
+    git clone -q https://github.com/jacksonliam/mjpg-streamer.git /home/pi/mjpg-streamer
+    echo "\e[1m\e[36mInstalling mjpg streamer\e[0m\e[39m"
+    cd /home/pi/mjpg-streamer/mjpg-streamer-experimental
+    export LD_LIBRARY_PATH=.
+    make --silent
+    sudo make --silent install
+    cd /home/pi
+    sudo rm -rf mjpg-streamer
+fi
+if [ "$SETUP_LOCAL_ACCESS" != 'false' ]; then
+    echo "\e[1m\e[36mSetting up accesss via https://aquarium.local\e[0m\e[39m"
+    sudo hostnamectl set-hostname aquarium
+    [ ! -f /etc/hostname ] && sudo echo "aquarium" > /etc/hostname
+    sudo sed -i 's/raspberrypi/aquarium/g' /etc/hosts
+fi
+popd
 echo "\e[1m\e[32mDone installing OctoPrint!\e[0m\e[39m"
